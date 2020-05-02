e55e0ed2b505f0f794ad174d4b1cf5b36545716c
Dominik Gruntz <dominik.gruntz@fhnw.ch>
2020-05-02 22:47:50 +0200

    Feedback to assignment 03 (Servlet)

--- src/main/java/bank/servlet/ServletDriver.java
+++ src/main/java/bank/servlet/ServletDriver.java
@@ -27,12 +27,19 @@ public class ServletDriver implements BankDriver {
 
 	private Bank bank;
 	private String path; // TODO: what is the path
+	// XXX das ist der Pfad auf den Server, also die URL, wird in sendRequest verwendet um auf den Dienst zuzugreifen:
+	//				.uri(new URI("http://" + path + "?" + query))
 	private HttpClient httpClient = HttpClient.newHttpClient();
 
 	@Override
 	public void connect(String[] args) throws IOException {
 		bank = new ServletBank(new ServletHandler());
 		// TODO: connect to hostname & port
+		// XXX HTTP ist ein zustandsloses Protokoll, daher wird (hier im connect) keine verbindung aufgebaut,
+		//     die Ausgabe "connected" ist daher verwirrend.
+		//     
+		//	   Wemm Sie mit Comamnds arbeiten, dann können Sie direkt eine Instanz der Klasse CommandBank 
+		//     erzeugen, die KLasse ServletBank braucht es dann nicht.
 		System.out.println("connected to " + args[0] + ":" + args[1]);
 	}
 
@@ -58,6 +65,8 @@ public class ServletDriver implements BankDriver {
 			throw new InternalError(e.getMessage());
 		}
 		// TODO: what Object is client?
+		// XXX:  Diese Frage habe ich nicht verstanden. Das "sendRequest" schickt ein GET Request an den Server und gibt die Antwort zurück.
+		//       Die Instanz httpclient ist oben erzeugt worden.
 		HttpResponse<Stream<String>> response = null;
 		try {
 			response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofLines());
@@ -68,8 +77,10 @@ public class ServletDriver implements BankDriver {
 	}
 
 	private class ServletBank extends CommandBank {
-
-		private String number;
+		// XXX Erweiterung von Command-Bank macht hier keinen Sinn, d.h. wenn die Methode sendRequest verwendet wird welche die Kommandos per GET Request an den Server überträgt.
+		
+		
+		private String number; // XXX diese Deklaration macht hier keinen Sinn.
 
 		public ServletBank(CommandHandler h) {
 			super(h);
@@ -87,6 +98,10 @@ public class ServletDriver implements BankDriver {
 		
 		@SuppressWarnings("unused")
 		private class Account extends CommandBank.AccountProxy {
+			// XXX jetzt haben wir irgendwie ein Durcheinander, d.h. werden die im Paket bank.command definierten Command-Objekte
+			//     an den Server geschickt (dann würde ich POST requests verwenden) oder handelt es sich hier um eine andere
+			//     Implementierung welche die Kommandos (wie hier unten im deposit implementiert) per GET-Requests an den
+			//     Server schickt.
 			
 			private Account(String number) {
 				super(number);
@@ -94,6 +109,11 @@ public class ServletDriver implements BankDriver {
 
 			public void withdraw(double amount) throws IOException, OverdrawException, InactiveException {
 				// TODO: what is number?
+				// XXX   Das ist auch eine Folge des oben erwähnten Durcheinanders. number ist die Konto-Nummer,
+				//       und die wird in der Basisklasse deklariert, aber eigentlich müssen Sie nicht von dieser
+				//       ableiten; also: number ist das Feld dieses Kontos, und das müssten Sie in der KLasse Account
+				//       deklarieren.
+				
 				Stream<String> lines = sendRequest(
 						"action=accountWithdraw" + "&number=" + encode(number) + "&amount=" + encode("" + amount));
 				String response = lines.findFirst().get();
@@ -112,6 +132,8 @@ public class ServletDriver implements BankDriver {
 	}
 	
 	private class ServletHandler implements CommandHandler {
+		// XXX diese ist der Handler der beim Erzeugen einer Instanz der Command-Bank übergeben wird.
+		//     Dieser schickt die serialisierten Commands per POST an den Server.
 
 		@Override
 		public Command handle(Command request) throws IOException {
@@ -131,6 +153,7 @@ public class ServletDriver implements BankDriver {
 				httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofInputStream());
 			} catch (IOException | InterruptedException e1) {
 				// TODO correct exception?
+				// XXX ich würde hier bei der IOException sicher eine IOException werfen, und wohl auch bei der InterruptedException.
 //				throw new RuntimeException(e1);
 				throw new InternalError(e1.getMessage());
 			}
@@ -139,6 +162,11 @@ public class ServletDriver implements BankDriver {
 				return (Command) in.readObject();
 			} catch (ClassNotFoundException | IOException e2) {
 				// TODO correct exception?
+				// XXX ja, das ist beides möglich, ich würde allerdings bei der IOException eine IOException werfen.
+				//     InternalError steht für Fehler der JVM:
+				//					 * Thrown to indicate some unexpected internal error has occurred in
+				//					 * the Java Virtual Machine.
+				//     das pass nicht wirklich, daher würde ich in diesem Fall eine RuntimeException werfen.
 //				throw new RuntimeException(e2);
 				throw new InternalError(e2.getMessage());
 			}
@@ -151,6 +179,9 @@ public class ServletDriver implements BankDriver {
 			return java.net.URLEncoder.encode(url, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			// TODO: correct exception?
+			// XXX ist hier eigentlich egal, denn diese Exception wird nur geworfen wenn es das Encoding nicht gibt, aber UTF-8 gibt es.
+			//     Wenn, dann wäre es ein Programmierfehler und für diesen würde ich eine RuntimeException verwenden, vielleicht auch eine
+			//     konkrete wie IllegalArgumentException.
 //			throw new RuntimeException(e);
 			throw new InternalError(e.getMessage());
 		}
