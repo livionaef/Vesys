d9e707e8d3445d092c64e9e50a50f87d3bd60438
Livio Näf <livio.naef@students.fhnw.ch>
2020-05-02 14:37:35 +0200

    Implemented the Driver but added multiple TODO's
    ...where something is not clear yet

--- src/main/java/bank/servlet/ServletDriver.java
+++ src/main/java/bank/servlet/ServletDriver.java
@@ -1,25 +1,19 @@
 package bank.servlet;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.UnsupportedEncodingException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetSocketAddress;
-import java.net.SocketTimeoutException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpRequest.Builder;
 import java.net.http.HttpResponse;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import javax.servlet.http.HttpServlet;
 import bank.Bank;
 import bank.BankDriver;
 import bank.InactiveException;
@@ -33,17 +27,18 @@ public class ServletDriver implements BankDriver {
 
 	private Bank bank;
 	private String path;
-	private HttpServlet client; // ??? where does client come from?
+	private HttpClient httpClient = HttpClient.newHttpClient();
 
 	@Override
 	public void connect(String[] args) throws IOException {
 		bank = new ServletBank(new ServletHandler());
+		// TODO: connect to hostname & port
 		System.out.println("connected to " + args[0] + ":" + args[1]);
 	}
 
 	@Override
 	public void disconnect() throws IOException {
-		bank = null; // bank auf null setzen
+		bank = null;
 		System.out.println("disconnected");
 	}
 
@@ -53,8 +48,24 @@ public class ServletDriver implements BankDriver {
 	}
 
 	private Stream<String> sendRequest(String query) throws IOException {
-		HttpRequest request = HttpRequest.newBuilder().uri(new URI("http://" + path + "?" + query)).GET().build();
-		HttpResponse<Stream<String>> response = client.send(request, HttpResponse.BodyHandlers.ofLines());
+		HttpRequest httpRequest = null;
+		try {
+			httpRequest = HttpRequest.newBuilder()
+					.uri(new URI("http://" + path + "?" + query))
+					.GET()
+					.build();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// TODO: what Object is client?
+		HttpResponse<Stream<String>> response = null;
+		try {
+			response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofLines());
+		} catch (IOException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return response.body();
 	}
 
@@ -75,25 +86,33 @@ public class ServletDriver implements BankDriver {
 			Stream<String> lines = sendRequest("action=createAccount&owner=encode(owner)");
 			return lines.findFirst().map(id -> "".equals(id) ? null : id).orElseGet(() -> null);
 		}
-
+		
 		@SuppressWarnings("unused")
-		public void withdraw(double amount) throws IOException, OverdrawException, InactiveException {
-			Stream<String> lines = sendRequest(
-					"action=accountWithdraw" + "&number=" + encode(number) + "&amount=" + encode("" + amount));
-			String response = lines.findFirst().get();
-			if (!response.equals("")) {
-				var reason = response.substring(response.indexOf(":"));
-				if (response.contains("OverdrawException")) {
-					throw new OverdrawException(reason);
-				} else if (response.contains("InactiveException")) {
-					throw new InactiveException(reason);
-				} else if (response.contains("IllegalArgumentException")) {
-					throw new IllegalArgumentException(reason);
+		private class Account extends CommandBank.AccountProxy {
+			
+			private Account(String number) {
+				super(number);
+			}
+
+			public void withdraw(double amount) throws IOException, OverdrawException, InactiveException {
+				// TODO: what is number?
+				Stream<String> lines = sendRequest(
+						"action=accountWithdraw" + "&number=" + encode(number) + "&amount=" + encode("" + amount));
+				String response = lines.findFirst().get();
+				if (!response.equals("")) {
+					var reason = response.substring(response.indexOf(":"));
+					if (response.contains("OverdrawException")) {
+						throw new OverdrawException(reason);
+					} else if (response.contains("InactiveException")) {
+						throw new InactiveException(reason);
+					} else if (response.contains("IllegalArgumentException")) {
+						throw new IllegalArgumentException(reason);
+					}
 				}
 			}
 		}
 	}
-
+	
 	private class ServletHandler implements CommandHandler {
 
 		@Override
@@ -102,26 +121,37 @@ public class ServletDriver implements BankDriver {
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 			ObjectOutputStream out = new ObjectOutputStream(baos);
 			out.writeObject(request);
-			out.flush(); // never forget
+			out.flush();
 			out.close();
-			
+
+			// SEND REQUEST & RECEIVE RESPONSE
 			HttpRequest httpRequest = HttpRequest.newBuilder()
 					.POST(HttpRequest.BodyPublishers.ofByteArray(baos.toByteArray()))
 					.build();
-			// ??? what is client?
-			HttpResponse<InputStream> response = client.send(request, HttpResponse.BodyHandlers.ofInputStream());
-			ObjectInputStream in = new ObjectInputStream(response.body());
-			return (Command) in.readObject();
+			HttpResponse<InputStream> httpResponse = null;
+			try {
+				httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofInputStream());
+			} catch (IOException | InterruptedException e1) {
+				// TODO correct exception?
+				throw new RuntimeException(e1);
+			}
+			ObjectInputStream in = new ObjectInputStream(httpResponse.body());
+			try {
+				return (Command) in.readObject();
+			} catch (ClassNotFoundException | IOException e2) {
+				// TODO correct exception?
+				throw new RuntimeException(e2);
+			}
 		}
 	}
 
+	// TODO: Should it be implemented like this?
 	public String encode(String url) {
-		String encodedUrl = null;
 		try {
-			encodedUrl = java.net.URLEncoder.encode(url, "UTF-8");
+			return java.net.URLEncoder.encode(url, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
+			// TODO: correct exception?
+			throw new RuntimeException(e);
 		}
-		return encodedUrl;
 	}
 }
